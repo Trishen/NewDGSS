45809ce242b8cbe2e8af9fe5dca42124059967bc
Trishen <trishendragon@yahoo.com>
2020-07-16 23:20:11 +0200

    created project

---
+++ Group21/Group21/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Group21.Startup))]
+namespace Group21
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
