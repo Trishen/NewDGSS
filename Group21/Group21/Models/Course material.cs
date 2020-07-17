ff7e319151b4614aa3a9828a38d0887e2c2084c5
DR KAPOFU <DR KAPOFU@BLESSINGS>
2020-07-17 12:28:06 +0200

    redo

---
+++ Group21/Group21/Models/Course material.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+
+namespace Group21.Models
+{
+    public class Course_material
+    {
+
+        [Key]
+        [DatabaseGenerated(System.ComponentModel.DataAnnotations.Schema.DatabaseGeneratedOption.Identity)]
+        [Required(ErrorMessage = "Enter course id")]
+        [Display(Name = "Id Number: ")]
+        public string courseID { get; set; }
+
+
+
+        [Required(ErrorMessage = "Enter course title")]
+        [Display(Name = "title: ")]
+        public string title { get; set; }
+
+
+        [Required(ErrorMessage = "Enter student id")]
+        [Display(Name = "Id Number: ")]
+        public virtual Student StID { get; set; }
+
+        [Required(ErrorMessage = "Enter subject id")]
+        [Display(Name = "subject id: ")]
+        public virtual sub SubID { get; set; }
+
+        [Required(ErrorMessage = "Enter staff id")]
+        [Display(Name = "Id Number: ")]
+        public virtual Staff StaffID { get; set; }
+
+
+    }
+}
+}
+}
