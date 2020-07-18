324811d2b0db2981a5869d569b76fd41ce0b4043
L Kabishi (21854425) <21854425@dut4life.ac.za>
2020-07-18 04:08:15 +0200

    Added subject class

--- Group21/Group21/Models/sub.cs
+++ Group21/Group21/Models/sub.cs
@@ -1,6 +1,29 @@
-﻿namespace Group21.Models
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+
+
+namespace Group21.Models
 {
     public class sub
     {
+    
+        [Key]
+        [Required(ErrorMessage = "Enter subject ID")]
+        [Display(Name = "Id")]
+        public int subID { get; set; }
+
+        [Required(ErrorMessage = "Enter subject name")]
+        [Display(Name = "Name")]
+        public string subName { get; set; }
+
+        [Required(ErrorMessage = "Enter subject requirements")]
+        [Display(Name = "Requirements")]
+        public string subRequirement { get; set; }
+
+
     }
 }
